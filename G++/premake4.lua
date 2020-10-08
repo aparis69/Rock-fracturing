4c20870ff1974cdc4cf9924b801bf1cf3c642e19
aparis69 <axel.paris69@gmail.com>
2020-10-08 16:39:19 +0200

    premake file

---
+++ G++/premake4.lua
@@ -0,0 +1,34 @@
+solution "RockFracturing"
+	configurations { "Debug", "Release" }
+
+	platforms { "x64" }
+	
+	includedirs { ".", "../Code/Include", "/usr/include/" }
+
+	rootDir = path.getabsolute("../")
+	
+	configuration "Debug"
+		targetdir "./Out/Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+
+	configuration "Release"
+		targetdir "./Out/Release"
+		flags { "OptimizeSpeed" }
+
+	configuration "linux"
+		buildoptions { "-mtune=native -march=native" }
+		buildoptions { "-std=c++14" }
+		buildoptions { "-w" }
+		buildoptions { "-flto -g"}
+		buildoptions { "-fopenmp"}
+		linkoptions { "-flto"}
+		linkoptions { "-g"}
+
+fileList = { rootDir .. "/Code/Source/*.cpp", rootDir .. "/Code/Include/*.h" }
+
+project("RockFracturing")
+	language "C++"
+	kind "ConsoleApp"
+	targetdir "Out"
+files ( fileList )
